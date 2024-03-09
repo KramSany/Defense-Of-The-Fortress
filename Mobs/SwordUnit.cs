2012ae94d45f7a183b71b5ed7217eb0a96bf11a6
KramSany <friman.kop@bk.ru>
2024-03-10 02:42:04 +0300

    0.1a hotfix
    Мелкие изменения.
    Исправил мелкие баги с юнитами игроками и базой противника. Добавлены новые анимации.

--- Mobs/SwordUnit.cs
+++ Mobs/SwordUnit.cs
@@ -4,76 +4,94 @@ using System.Diagnostics;
 
 public partial class SwordUnit : CharacterBody2D
 {
-    // сделать шлавный класс под всех юнитов
     private AnimatedSprite2D _animatedSprite;
     public float HelathPoint = 100.0f;
-    public float DamagePerSecond = 10.0f;
-    // Скорость движения игрока
-    public float MoveSpeed = 50.0f;
+    public float DamageUnit = 5;
+    // Скорость движения юнита
+    public float MoveSpeed = 40.0f;
 	private Vector2 gravity = new Vector2(0, 800.0f);
 
-    public bool Alive { get; internal set; }
+    public bool isEnemy = false;
+
+    private void DeathUnit()
+    {
+        if (HelathPoint <= 0) QueueFree();
+        
+    }
 
     public override void _Ready()
     {
         _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
     }
-
-    // Вызывается каждый кадр
 	private void ApplyGravity()
     {
+        
 		Vector2 velocity = Velocity;
-        // Применяем гравитацию к вертикальной составляющей скорости
+        
         velocity.Y += gravity.Y * (float)GetProcessDeltaTime();
+        
 		Velocity = velocity;
     }
     public override void _Process(double delta)
     {
-		
-		// ApplyGravity();
-        // Движение игрока вправо
+        DeathUnit();
+		Debug.Print($"{HelathPoint}");
+		ApplyGravity();
+        if (MoveSpeed <= 0 && isEnemy != true)
+        {
+             _animatedSprite.Play("Idle");
+        }
+        else if (MoveSpeed > 0)
+        {
+            _animatedSprite.Play("Walk");
+        }
 		MoveAndSlide();
         MoveRight();
+        
     }
 
-    internal void TakeDamage()
+    internal void TakeDamage(float damage)
     {
-        throw new NotImplementedException();
+        HelathPoint-=damage;
     }
 
 
     private void MoveRight()
+{
+    if (MoveSpeed != 0.0f)
     {
         Vector2 currentPosition = Position;
         Vector2 newPosition = currentPosition + new Vector2(MoveSpeed * (float)GetProcessDeltaTime(), 0);
         Position = newPosition;
     }
+}
 
-    public void _on_area_2d_body_entered(Node2D body)
+    private void _on_area_2d_body_entered(Node2D node)
     {
-
-        if (body is CharacterBody2D)
+        if (node is CharacterBody2D)
         {
-            if (body is AxeUnit)
+            
+            if (node is AxeUnit)
             {
-                MoveSpeed = 40.0f;
-                Debug.Print($"{MoveSpeed}");
+                MoveSpeed = 0.0f;
+                
 
             }
             
         }
-        else if (body is Node2D)
+        else if (node is Node2D)
         {
-            Debug.Print("lol");
-            _animatedSprite.Play("Idle");
+            isEnemy = true;
             MoveSpeed = 0.0f;
+            _animatedSprite.Play("Atack");
+            Debug.Print($"penis");
         }
     }
 
-    public void _on_area_2d_body_exited(Node2D body)
+    private void _on_area_2d_body_exited(Node2D node)
     {
-        MoveSpeed = 50.0f;
+        MoveSpeed = 40.0f;
+
     }
-    
 	
 }
