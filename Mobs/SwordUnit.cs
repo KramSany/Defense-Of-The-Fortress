d8738f65efa16786d5035e17d676fc686b6355da
KramSany <friman.kop@bk.ru>
2024-03-11 03:28:32 +0300

    0.1b build
    Мелкие изменения.
    Реализован базовый класс для всех юнитов. Исправлен баг анимацией ходьбы и атаки.

--- Mobs/SwordUnit.cs
+++ Mobs/SwordUnit.cs
@@ -2,96 +2,19 @@ using Godot;
 using System;
 using System.Diagnostics;
 
-public partial class SwordUnit : CharacterBody2D
+public partial class SwordUnit : BaseUnit
 {
-    private AnimatedSprite2D _animatedSprite;
-    public float HelathPoint = 100.0f;
-    public float DamageUnit = 5;
-    // Скорость движения юнита
-    public float MoveSpeed = 40.0f;
-	private Vector2 gravity = new Vector2(0, 800.0f);
-
-    public bool isEnemy = false;
-
-    private void DeathUnit()
+    public SwordUnit()
     {
-        if (HelathPoint <= 0) QueueFree();
-        
+        Health = 100.0f; // Устанавливаем начальное значение HP для SwordUnit
     }
-
-    public override void _Ready()
+   private void _on_area_2d_body_entered(Node2D node)
     {
-        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    }
-	private void ApplyGravity()
-    {
-        
-		Vector2 velocity = Velocity;
-        
-        velocity.Y += gravity.Y * (float)GetProcessDeltaTime();
-        
-		Velocity = velocity;
-    }
-    public override void _Process(double delta)
-    {
-        DeathUnit();
-		Debug.Print($"{HelathPoint}");
-		ApplyGravity();
-        if (MoveSpeed <= 0 && isEnemy != true)
-        {
-             _animatedSprite.Play("Idle");
-        }
-        else if (MoveSpeed > 0)
-        {
-            _animatedSprite.Play("Walk");
-        }
-		MoveAndSlide();
-        MoveRight();
-        
-    }
-
-    internal void TakeDamage(float damage)
-    {
-        HelathPoint-=damage;
-    }
-
-
-    private void MoveRight()
-{
-    if (MoveSpeed != 0.0f)
-    {
-        Vector2 currentPosition = Position;
-        Vector2 newPosition = currentPosition + new Vector2(MoveSpeed * (float)GetProcessDeltaTime(), 0);
-        Position = newPosition;
-    }
-}
-
-    private void _on_area_2d_body_entered(Node2D node)
-    {
-        if (node is CharacterBody2D)
-        {
-            
-            if (node is AxeUnit)
-            {
-                MoveSpeed = 0.0f;
-                
-
-            }
-            
-        }
-        else if (node is Node2D)
-        {
-            isEnemy = true;
-            MoveSpeed = 0.0f;
-            _animatedSprite.Play("Atack");
-            Debug.Print($"penis");
-        }
+        OnAreaEntered(node);
     }
 
     private void _on_area_2d_body_exited(Node2D node)
     {
-        MoveSpeed = 40.0f;
-
+        OnAreaExited(node);
     }
-	
 }
